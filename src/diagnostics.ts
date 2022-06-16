48abffe54646ef697b841b1553ece307de1fd593
Luis Sousa <lv-ss@hotmail.com>
2022-06-16 03:04:15 -0300

    feat: add find unused blocks command

--- src/diagnostics.ts
+++ src/diagnostics.ts
@@ -1,26 +1,25 @@
-import * as vscode from 'vscode';
-import {glob} from "glob";
-import { parse } from "jsonc-parser";
-import fs = require("fs");
+import * as vscode from "vscode"
+import { glob } from "glob"
+import { parse } from "jsonc-parser"
+import fs = require("fs")
 
 /** Code that is used to associate diagnostic entries with code actions. */
-export const UNUSED_BLOCK = 'unused_block';
-export const NO_EXPLICIT_USE_BLOCKS = ["store.", "header", "header.", "footer"];
+export const UNUSED_BLOCK = "unused_block"
+export const NO_EXPLICIT_USE_BLOCKS = ["store.", "header", "header.", "footer"]
 
 type BlockFormat = {
 	props?: {
 		Then: string
 		Else: string
 		[key: string]: string
-	},
+	}
 	blocks?: string[]
 	children?: string[]
-};
+}
 
 type GenericObject = {
 	[key: string]: BlockFormat
-};
-
+}
 
 /** String to detect in the text document. */
 
@@ -29,129 +28,196 @@ type GenericObject = {
  * @param doc text document to analyze
  * @param blocksDiagnostics diagnostic collection
  */
-export function refreshDiagnostics(doc: vscode.TextDocument, blocksDiagnostics: vscode.DiagnosticCollection): void {
-	const diagnostics: vscode.Diagnostic[] = [];
-	
-	const currentFilePath = doc.fileName.replace(/\\/g, "/");
+export function refreshDiagnostics(
+	// doc: vscode.TextDocument,
+	blocksDiagnostics: vscode.DiagnosticCollection
+): void {
+	console.log("rodou")
 
-	const [folder] = vscode?.workspace?.workspaceFolders || [] as vscode.WorkspaceFolder[];
+	// const currentFilePath = doc.fileName.replace(/\\/g, "/")
 
-	const files = glob.sync(folder?.uri?.fsPath + "/**/store/blocks/**/*.{json,jsonc}");
+	const [folder] =
+		vscode?.workspace?.workspaceFolders || ([] as vscode.WorkspaceFolder[])
 
-	const allJsons = files.reduce((content: any, file: any, i: any) => {
-		const x = fs.readFileSync(
-			file,
-			{ encoding: "utf-8" }
-		);
-		if (file.endsWith(".jsonc")) {
-			return { ...content, ...parse(x) };
-		}
-		return { ...content, ...JSON.parse(x) };
-	}, {});
+	const files = glob.sync(
+		folder?.uri?.fsPath + "/**/store/blocks/**/*.{json,jsonc}"
+	)
 
-	if(!files.includes(currentFilePath)) {return;};
+	const { allJSONs, jsonFiles } = files.reduce(
+		(content: any, file: any) => {
+			const x = fs.readFileSync(file, { encoding: "utf-8" })
+			const contentObj = file.endsWith(".jsonc") ? parse(x) : JSON.parse(x)
 
-	const currentDocumentText = doc.getText();
-
-	const currentDocumentObject = parse(currentDocumentText);
-
-	const ids = checkKey(currentDocumentObject, allJsons);
-
-	const filteredIds = ids.filter((id: string | undefined) =>{ 
-	 return id !== undefined && !NO_EXPLICIT_USE_BLOCKS.some((blockId) => id.startsWith(blockId) ); 
-	});
-
-	filteredIds.forEach((id) => {
-		for (let lineIndex = 0; lineIndex < doc.lineCount; lineIndex++) {
-			const lineOfText = doc.lineAt(lineIndex);
-			if (lineOfText.text.includes( `"${id}"`)) {
-				diagnostics.push(createDiagnostic(doc, lineOfText, lineIndex, `"${id}"`));
+			return {
+				allJSONs: { ...content.allJSONs, ...contentObj },
+				jsonFiles: [
+					...content.jsonFiles,
+					{ filePath: file, content: contentObj }
+				]
 			}
-		}
-	});
+		},
+		{ allJSONs: {}, jsonFiles: [] }
+	)
 
-	blocksDiagnostics.set(doc.uri, diagnostics);
-}
+	console.log(allJSONs)
 
-function createDiagnostic(doc: vscode.TextDocument, lineOfText: vscode.TextLine, lineIndex: number, word: string): vscode.Diagnostic {
-	const index = lineOfText.text.indexOf(word);
+	// if (!files.includes(currentFilePath)) {
+	// 	return
+	// }
 
-	// create range that represents, where in the document the word is
-	const range = new vscode.Range(lineIndex, index, lineIndex, index + word.length);
+	// const currentDocumentText = doc.getText()
 
-	const diagnostic = new vscode.Diagnostic(range, "Unused block",
-		vscode.DiagnosticSeverity.Warning);
-	diagnostic.code = UNUSED_BLOCK;
-	return diagnostic;
-}
+	// const currentDocumentObject = parse(currentDocumentText)
 
-export function subscribeToDocumentChanges(context: vscode.ExtensionContext, blocksDiagnostics: vscode.DiagnosticCollection): void {
-	const configs = vscode.workspace.getConfiguration('vtexiointellisense');
+	const checkedFiles = jsonFiles.map(({ filePath, content }) => ({
+		filePath,
+		ids: checkKey(content, allJSONs)
+	}))
 
-	if(configs.allowsUnusedBlocks) {return;}
+	const filesWithFilteredIds = checkedFiles.map(({ filePath, ids }) => ({
+		filePath,
+		ids: ids.filter((id) => {
+			return !NO_EXPLICIT_USE_BLOCKS.some((blockId) => id.startsWith(blockId))
+		})
+	}))
 
-	if (vscode.window.activeTextEditor) {
-		refreshDiagnostics(vscode.window.activeTextEditor.document, blocksDiagnostics);
-	}
-	context.subscriptions.push(
-		vscode.window.onDidChangeActiveTextEditor(editor => {
-			if (editor) {
-				refreshDiagnostics(editor.document, blocksDiagnostics);
+	filesWithFilteredIds.map(async ({ filePath, ids }) => {
+		const diagnostics: vscode.Diagnostic[] = []
+
+		const uri = vscode.Uri.file(filePath)
+		const doc = await vscode.workspace.openTextDocument(uri)
+		console.log(uri)
+
+		ids.forEach((id) => {
+			for (let lineIndex = 0; lineIndex < doc.lineCount; lineIndex++) {
+				const lineOfText = doc.lineAt(lineIndex)
+				if (lineOfText.text.includes(`"${id}"`)) {
+					diagnostics.push(
+						createDiagnostic(doc, lineOfText, lineIndex, `"${id}"`)
+					)
+				}
 			}
 		})
-	);
 
-	context.subscriptions.push(
-		vscode.workspace.onDidChangeTextDocument(e => refreshDiagnostics(e.document, blocksDiagnostics))
-	);
+		blocksDiagnostics.set(doc.uri, diagnostics)
+	})
 
-	context.subscriptions.push(
-		vscode.workspace.onDidCloseTextDocument(doc => blocksDiagnostics.delete(doc.uri))
-	);
-
+	console.log("finalizou")
 }
 
-// TODO add verify if prop starts with [A-Z]
-const checkUse = (id: string, json: GenericObject) => {
-  let uses : any;
-  Object.entries(json).forEach(([_key, value]) => {
+function createDiagnostic(
+	doc: vscode.TextDocument,
+	lineOfText: vscode.TextLine,
+	lineIndex: number,
+	word: string
+): vscode.Diagnostic {
+	const index = lineOfText.text.indexOf(word)
 
-    if ("children" in value) {
-      if (!uses) {
-        uses = value.children?.includes(id);
-      }
-    }
-    if ("blocks" in value) {
-      if (!uses) {
-        uses = value.blocks?.includes(id);
-      }
-    }
-		if ("props" in value ) {
-			if (value.props?.hasOwnProperty("Then")) {
-				if (!uses) {
-					uses = value.props.Then.includes(id);
-				}
+	// create range that represents, where in the document the word is
+	const range = new vscode.Range(
+		lineIndex,
+		index,
+		lineIndex,
+		index + word.length
+	)
+
+	const diagnostic = new vscode.Diagnostic(
+		range,
+		"Unused block",
+		vscode.DiagnosticSeverity.Warning
+	)
+	diagnostic.code = UNUSED_BLOCK
+	return diagnostic
+}
+
+export function subscribeToDocumentChanges(
+	context: vscode.ExtensionContext,
+	blocksDiagnostics: vscode.DiagnosticCollection
+): void {
+	const configs = vscode.workspace.getConfiguration("vtexiointellisense")
+
+	// if (configs.allowsUnusedBlocks) {
+	// 	return
+	// }
+
+	if (vscode.window.activeTextEditor) {
+		refreshDiagnostics(
+			// vscode.window.activeTextEditor.document,
+			blocksDiagnostics
+		)
+	}
+	context.subscriptions.push(
+		vscode.window.onDidChangeActiveTextEditor((editor) => {
+			if (editor) {
+				refreshDiagnostics(blocksDiagnostics)
 			}
-			if (value.props?.hasOwnProperty("Else")) {
-				if (!uses) {
-					uses = value.props.Else.includes(id);
-				}
-			}
-			// uses = Object.entries(value.props).some(([key, value]) =>
-    	// 	 key.startsWith('[A-Z]') && value.includes(id)
-			// );
+		})
+	)
+
+	// context.subscriptions.push(
+	// 	vscode.workspace.onDidChangeTextDocument((e) =>
+	// 		refreshDiagnostics(e.document, blocksDiagnostics)
+	// 	)
+	// )
+
+	// context.subscriptions.push(
+	// 	vscode.workspace.onDidCreateFiles()
+	// )
+	// //
+	context.subscriptions.push(
+		vscode.workspace.onDidCloseTextDocument((doc) =>
+			blocksDiagnostics.delete(doc.uri)
+		)
+	)
+}
+
+const checkUse = (id: string, json: GenericObject) => {
+	const uses = Object.entries(json).some(([_key, value]) => {
+		let inChildren,
+			inBlocks,
+			inProp,
+			inThen,
+			inElse = false
+
+		if (value.children) {
+			inChildren = value.children.includes(id)
 		}
-	});
-  if (!uses) {
-    return id;
-  }
-};
+
+		if (value.blocks) {
+			inBlocks = value.blocks.includes(id)
+		}
+
+		if (value.props) {
+			if (value.props.hasOwnProperty("Then")) {
+				inThen = value.props.Then.includes(id)
+			}
+			if (value.props.hasOwnProperty("Else")) {
+				inElse = value.props.Else.includes(id)
+			}
+
+			inProp = Object.entries(value.props).some(
+				([key, value]) => key.match(/^[A-Z]/) && value === id
+			)
+		}
+		return inChildren || inBlocks || inThen || inElse || inProp
+	})
+
+	if (!uses) {
+		return id
+	}
+}
 
 const checkKey = (currentDocument: any, json: any) => {
+	const blockIds = Object.entries(currentDocument).reduce<string[]>(
+		(acc, [key, _value]) => {
+			const id = checkUse(key, json)
+			if (id) {
+				return [...acc, id]
+			}
+			return acc
+		},
+		[]
+	)
 
-  const blockids = Object.entries(currentDocument).map(([key, _value]) =>
-    checkUse(key, json)
-  );
-
-  return blockids;
-};
+	return blockIds
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,40 @@
-import * as vscode from 'vscode';
-import { subscribeToDocumentChanges } from './diagnostics';
-
+import * as vscode from "vscode"
+import {
+	// findDuplicatedBlocks,
+	refreshDiagnostics,
+	subscribeToDocumentChanges
+} from "./diagnostics"
 
 export function activate(context: vscode.ExtensionContext) {
-	const blocksDiagnostics = vscode.languages.createDiagnosticCollection("blocks");
-	context.subscriptions.push(blocksDiagnostics);
+	const blocksDiagnostics =
+		vscode.languages.createDiagnosticCollection("blocks")
+	const command = vscode.commands.registerCommand(
+		"vtexiointellisense.findUnusedBlocks",
+		() => {
+			if (!vscode.window.activeTextEditor) {
+				return
+			}
+			refreshDiagnostics(
+				// vscode.window.activeTextEditor.document,
+				blocksDiagnostics
+			)
+		}
+	)
 
-	subscribeToDocumentChanges(context, blocksDiagnostics);
+	// vscode.commands.registerCommand(
+	// 	"vtexiointellisense.findDuplicatedBlocks",
+	// 	() => {
+	// 		if (!vscode.window.activeTextEditor) {
+	// 			return
+	// 		}
+	// 		findDuplicatedBlocks(
+	// 			// vscode.window.activeTextEditor.document,
+	// 			blocksDiagnostics
+	// 		)
+	// 	}
+	// )
+
+	context.subscriptions.push(blocksDiagnostics)
+
+	// subscribeToDocumentChanges(context, blocksDiagnostics)
 }
